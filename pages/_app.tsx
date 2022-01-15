337c2c827a259620ced5ad783d961773f9b6377b
derikbf <derikb@gmail.com>
2022-01-15 15:40:01 -0300

    add and config Bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-
 import Head from 'next/head';
-
 import '../styles/globals.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
