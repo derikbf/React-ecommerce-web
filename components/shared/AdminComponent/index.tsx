9a60a9d78056df853d0984d403c39626cdfbd49d
derikbf <derikb@gmail.com>
2022-01-16 11:27:40 -0300

    Admin Component

---
+++ components/shared/AdminComponent/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import AdminHeader from '../Header/AdminHeader';
+
+
+const AdminComponent: React.FC = ({children}) => {
+  return(
+    <Row className="mr-lg-4">
+      <Col lg={3}>
+          Menu Lateral
+      </Col>
+
+      <Col lg={9}>
+          <div className="d-flex flex-column sticky-footer-wrapper container">
+            
+            <AdminHeader name="Nome do User" />
+            
+            <div className="flex-fill text-center">
+                { children }
+            </div>
+          </div>
+      </Col>
+    </Row>
+  )
+}
+
+export default AdminComponent;
