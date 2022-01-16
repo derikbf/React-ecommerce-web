438e4997c784984ee2fe19ae07f751e3ef5d62fc
derikbf <derikb@gmail.com>
2022-01-16 14:49:13 -0300

    Created Component Delete Modal

---
+++ components/shared/AdminDeleteModal/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import styles from '../../../styles/AdminPanel.module.css';
+import { Modal, Row, Col } from 'react-bootstrap';
+import { faTrash, faTimes } from '@fortawesome/free-solid-svg-icons';
+import StyledButton from '../StyledButton';
+
+interface AdminDeleteModalProps {
+  show: boolean,
+  handleClose: (success?: boolean) => void,
+  target: String
+}
+
+const AdminDeleteModal: React.FC<AdminDeleteModalProps> = ({show, handleClose, target}) => {
+  return (
+    <Modal show={show} onHide={handleClose} className={styles.modal} animation={true}>
+      <Modal.Body className={styles.modal_body}>
+          Tem certeza que deseja excluir este {target}?
+
+      <Row>
+        <Col lg={6} xs>
+          <div onClick={() => handleClose(true)}>
+            <StyledButton icon={faTrash} action={"Excluir"} type_button="red" />
+          </div>
+        </Col>
+
+        <Col lg={6} xs>
+          <div onClick={() => handleClose(true)}>
+            <StyledButton icon={faTimes} action={"Cancelar"} type_button="blue" />
+          </div>
+        </Col>
+      </Row>
+
+
+      </Modal.Body>
+    </Modal>
+  )
+}
+
+export default AdminDeleteModal;
