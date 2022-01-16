b045f9eac9970e5160322d667f3f56b882089870
derikbf <derikb@gmail.com>
2022-01-15 21:59:06 -0300

    Updated page index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import MainComponent from '../components/shared/MainComponent';
+import { useRouter } from 'next/router';
 
 const Home: React.FC = () => {
-  return <h1>Hello World</h1>
+  const router = useRouter();
+
+  return (
+    <MainComponent>
+      <button onClick={ () => router.push('Auth/Login') }> </button>
+    </MainComponent>
+  )
 }
 
-export default Home
+export default Home;
